2f11a05d6c451d8ecb0aa548d67a3842efdca5b0
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2022-12-15 05:34:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -165,11 +165,11 @@
             await rpgen4.midiScheduler.play();
             scheduledToEnd(new Date(Date.now() + rpgen4.midiScheduler.scheduledTime + rpgen4.midiScheduler.duration).toTimeString());
         }).addClass('btn');
-        $('<dd>').appendTo(html);
         rpgen3.addBtn(html, '音色の初期化', async () => {
             rpgen4.midiScheduler.midiOutput.allChannels.programChange({data: {programChange: 0x00}});
             scheduledToEnd('音色を初期化した');
         }).addClass('btn');
+        $('<dd>').appendTo(html);
     }
     const makeMessageArrays = () => {
         const midiNoteArray = rpgen4.MidiNote.makeArray(g_midi);
